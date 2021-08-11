e8da7ef494a096203a308b6b5f7fc18582d9b1d5
Kamil Grajko <kamilgrajko@interia.pl>
2021-08-11 20:04:47 +0200

    change data format

--- weather.py
+++ weather.py
@@ -6,13 +6,13 @@ import json
 import os
 
 today = datetime.date.today()
-a = today.strftime("%d-%m-%Y")
+a = today.strftime("%Y-%m-%d")
 
 api_key = (sys.argv[1])
 date = (sys.argv[2]) if len(sys.argv) >= 3 else a
 list = {}
 
-d1 = datetime.datetime.strptime(date, "%d-%m-%Y")
+d1 = datetime.datetime.strptime(date, "%Y-%m-%d")
 d2 = d1 + datetime.timedelta(days=1)
 d1 = time.mktime(d1.timetuple())
 d2 = time.mktime(d2.timetuple())
