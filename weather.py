3484abd52b7ba38ba58b5105c640d7696f283567
Kamil Grajko <kamilgrajko@interia.pl>
2021-08-22 22:38:52 +0200

    create date converter

--- weather.py
+++ weather.py
@@ -4,3 +4,14 @@ import time
 import requests
 import json
 import os
+
+today = datetime.date.today()
+a = today.strftime("%Y-%m-%d")
+
+key_api = (sys.argv[1])
+date = (sys.argv[2]) if len(sys.argv) >= 3 else a
+
+d1 = datetime.datetime.strptime(date, "%Y-%m-%d")
+d2 = d1 + datetime.timedelta(days=1)
+d1 = time.mktime(d1.timetuple())
+d2 = time.mktime(d2.timetuple())
